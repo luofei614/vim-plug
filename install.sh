84130fa4e015702673933b5c462ca7a4f56cb327
for lynn <lynn@meihuapp.net>
2016-03-22 21:25:52 +0800

    config

--- install.sh
+++ install.sh
@@ -14,13 +14,14 @@ lnif() {
 echo "Step1: backing up current vim config"
 today=`date +%Y%m%d`
 #for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -e $i  ] && [ ! -L $i  ] && mv $i $i.$today; done
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.eslintrc.json $HOME/.vimrc.bundles; do [ -L $i  ] && unlink $i ; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.eslintrc.json $HOME/.tern-config $HOME/.vimrc.bundles; do [ -L $i  ] && unlink $i ; done
 
 
 echo "Step2: setting up symlinks"
-lnif $CURRENT_DIR/.vimrc $HOME/.vimrc
+lnif $CURRENT_DIR/config/vimrc $HOME/.vimrc
 lnif "$CURRENT_DIR/" "$HOME/.vim"
-lnif $CURRENT_DIR/.eslintrc.json $HOME/.eslintrc.json
+lnif $CURRENT_DIR/config/eslintrc.json $HOME/.eslintrc.json
+lnif $CURRENT_DIR/config/tern-config $HOME/.tern-config
 
 echo "Step3: update/install plugins using vim-plug"
 system_shell=$SHELL
@@ -32,5 +33,5 @@ export SHELL=$system_shell
 echo "Step4: install eslint"
 echo "It will take a long time, just be patient!"
 echo "npm i -g eslint --registry=http://registry.npm.taobao.org"
-npm i -g eslint eslint-plugin-babel eslint-plugin-react eslint-plugin-react-native babel-eslint  --registry=http://registry.npm.taobao.org
+npm i -g eslint eslint-plugin-babel eslint-plugin-react eslint-plugin-react-native babel-eslint eslint-config-airbnb  --registry=http://registry.npm.taobao.org
 echo "Install Done!"
