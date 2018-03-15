0b9e509a82bb696a7be3d2cd4d17ed6ae4425553
sherylynn <352281674@qq.com>
2018-03-15 16:02:04 +0800

    新增了proxy中go工具的fzf的安装，重新引入了fzf的搜索

--- proxy.bat
+++ proxy.bat
@@ -3,22 +3,27 @@ echo 开始安装依赖
 set http_proxy=http://127.0.0.1:8087
 set https_proxy=http://127.0.0.1:8087
 git config http.proxy http://127.0.0.1:8087
-go get -u github.com/klauspost/asmfmt/cmd/asmfmt
-go get -u github.com/derekparker/delve/cmd/dlv
-go get -u github.com/kisielk/errcheck
-go get -u github.com/davidrjenni/reftools/cmd/fillstruct
-go get -u -ldflags -H=windowsgui github.com/nsf/gocode
-go get -u github.com/rogpeppe/godef
-go get -u github.com/zmb3/gogetdoc
-go get -u golang.org/x/tools/cmd/goimports
-go get -u github.com/golang/lint/golint
-go get -u github.com/alecthomas/gometalinter
-go get -u github.com/fatih/gomodifytags
-go get -u golang.org/x/tools/cmd/gorename
-go get -u github.com/jstemmer/gotags
-go get -u golang.org/x/tools/cmd/guru
-go get -u github.com/josharian/impl
-go get -u github.com/dominikh/go-tools/cmd/keyify
-go get -u github.com/fatih/motion
+:: update=-u就是强制更新
+set update=
+go get %update% github.com/klauspost/asmfmt/cmd/asmfmt
+go get %update% github.com/derekparker/delve/cmd/dlv
+go get %update% github.com/kisielk/errcheck
+go get %update% github.com/davidrjenni/reftools/cmd/fillstruct
+go get %update% -ldflags -H=windowsgui github.com/nsf/gocode
+go get %update% github.com/rogpeppe/godef
+go get %update% github.com/zmb3/gogetdoc
+go get %update% golang.org/x/tools/cmd/goimports
+go get %update% github.com/golang/lint/golint
+go get %update% github.com/alecthomas/gometalinter
+go get %update% github.com/fatih/gomodifytags
+go get %update% golang.org/x/tools/cmd/gorename
+go get %update% github.com/jstemmer/gotags
+go get %update% golang.org/x/tools/cmd/guru
+go get %update% github.com/josharian/impl
+go get %update% github.com/dominikh/go-tools/cmd/keyify
+go get %update% github.com/fatih/motion
+
+:: fzf 依赖
+go get %update% github.com/junegunn/fzf
 EXIT /B
 ::goto :eof和 EXIT /B 都能返回到之前的call里 但是是不会返回到start里
