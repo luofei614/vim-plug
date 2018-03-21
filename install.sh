17dfe2bf61bb2fdb897d4b6f2f0f5c0f70416d0b
sherylynn <352281674@qq.com>
2018-03-21 16:38:22 +0800

    一些依赖

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ lnif() {
 }
 
 echo "Step0: install dependencies "
-sudo apt-get install silversearcher-ag
-sudo yum install the_silver_searcher
-brew install the_silver_searcher
+sudo apt-get install silversearcher-ag ctags
+sudo yum install the_silver_searcher ctags
+brew install the_silver_searcher ctags
 
 echo "Step1: backing up current vim config"
 today=`date +%Y%m%d`
